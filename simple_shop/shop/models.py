7c0159f4d8b8458e6936b4688023e2487dea3f95
mjwhodur <mjwhodur@gmail.com>
2018-10-08 16:12:47 +0200

    Added missing docstrings, file reformatted

--- simple_shop/shop/models.py
+++ simple_shop/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Client(models.Model):
@@ -51,6 +52,10 @@ class Product(models.Model):
     Model: Product
 
     Description:
+        Model holding data of products. This model does not contain information about product's prices.
+        This is because the seller may want to set different prices for different currencies.
+        However, system enables to automatically convert between currencies if price in current currency is not set
+
 
     Columns:
         Title: typeof django.db.models.CharField
@@ -61,11 +66,14 @@ class Product(models.Model):
     Image = models.CharField(max_length=500, null=True)
     Description = models.CharField(max_length=500)
 
+
 class Currency(models.Model):
     """
     Model: Currency
 
     Description:
+        Model holding data of currencies. Allows automatic conversion between currencies, if needed. If not, user has to
+        just set currency rate to 1 and it will be treated like base currency.
 
     Columns:
         Name: typeof django.db.models.CharField
@@ -76,6 +84,7 @@ class Currency(models.Model):
     Mnemonic = models.CharField(max_length=3)
     ExchangeRate = models.FloatField(default=1)
 
+
 class PriceList(models.Model):
     """
     Models: PriceList
@@ -83,54 +92,63 @@ class PriceList(models.Model):
     Description:
 
     Columns:
-        relatedProduct
-        relatedCurrency
+        relatedProduct: typeof django.db.models.ForeignKey in relation to shop.models.Product
+        relatedCurrency typeof django.db.models.ForeignKey in relation to shop.models.Currency
     """
     relatedProduct = models.ForeignKey(Product, on_delete=models.deletion.PROTECT)
     relatedCurrency = models.ForeignKey(Currency, on_delete=models.deletion.PROTECT)
+
 
 class DeliveryType(models.Model):
     """
     Model: DeliveryType
 
     Description:
+        Holds names of delivery types.
+        NOTE: The Price is preserved in DeliveryPrice model depending on Currency.
 
     Columns:
-        Name
+        Name: typeof django.db.models.CharField
     """
     Name = models.CharField(max_length=500)
+
 
 class DeliveryPrice(models.Model):
     """
     Model: DeliveryPrice
 
     Description:
+        Holds delivery prices in different currencies.
 
     Columns:
-        Name
-        Currency
-        Price
+        DeliveryType: typeof django.db.models.ForeignKey in relation to shop.models.DeliveryType
+        Currency: typeof django.db.models.ForeignKey in relation to shop.models.Currency
+        Price: typeof django.db.models.FloatField holding price data
     """
-    Name = models.CharField(max_length=500)
+    DeliveryType = models.ForeignKey(DeliveryType, on_delete=models.deletion.CASCADE)
     Currency = models.ForeignKey(Currency, on_delete=models.deletion.CASCADE)
     Price = models.FloatField(default=0)
+
 
 class Category(models.Model):
     """
     Model: Category
 
     Description:
+        Holds information about categories available in system.
 
     Columns:
-        Name
+        Name: typeof django.db.models.CharField, holding data of Category name
     """
     Name = models.CharField(max_length=50, null=False)
+
 
 class Order(models.Model):
     """
     Model: Order
 
     Description:
+        Holds basic data about placed orders.
 
     Columns:
         Number
@@ -143,30 +161,40 @@ class Order(models.Model):
     DeliveryType = models.ForeignKey(DeliveryType, on_delete=models.deletion.PROTECT)
     Currency = models.ForeignKey(Currency, on_delete=models.deletion.PROTECT)
 
+
 class OrderPositions(models.Model):
     """
     Model OrderPositions
 
     Description:
+        Holds detailed information about positions of the order
+        NOTE: The price may change if the staff change so we preserve the price and currency ordered
+              Deletion of product does not remove the order's positions. Orders placed before deletion
+              will be processed normally.
 
     Columns:
         Order
         Product
-        DeliveryType
+        Quantity
+        Price
     """
     Order = models.ForeignKey(Order, on_delete=models.deletion.CASCADE)
     Product = models.ForeignKey(Product, on_delete=models.deletion.PROTECT)
-    DeliveryType = models.ForeignKey(DeliveryType, on_delete=models.deletion.PROTECT)
+    Quantity = models.IntegerField(default=0)
+    Price = models.FloatField(default=0)
+    Currency = models.ForeignKey(Currency, on_delete=models.deletion.CASCADE)
+
 
 class WishList(models.Model):
     """
     Model WishList
 
     Description:
+        Holds wishlist.
 
     Columns:
-        User
-        Product
+        User: typeof django.db.models.ForeignKey with relation to shop.models.Client
+        Product: typeof django.db.models.ForeignKey with relation to shop.models.Product
     """
     User = models.ForeignKey(Client, on_delete=models.deletion.CASCADE)
     Product = models.ForeignKey(Product, on_delete=models.deletion.CASCADE)
