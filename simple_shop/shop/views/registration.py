0b8fb5ade528bccb69850876144f118f1ac92c20
Michal Weglowski-Hodur <mjwhodur@gmail.com>
2018-10-08 12:58:01 +0200

    Model

--- simple_shop/shop/views/registration.py
+++ simple_shop/shop/views/registration.py
@@ -3,3 +3,4 @@ from . import paint
 
 def registration_view(request):
     return HttpResponse('This feature has not yet been implemented')
+    #return paint.registration_view(request, context, status)
