25773b803eaf8d964aefd1343032b3f57a95a8ce
mjwhodur <mjwhodur@gmail.com>
2018-10-11 12:27:11 +0200

    Warnings

--- ninja/ninja/urls.py
+++ ninja/ninja/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from ninja import api, shop, panel
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api.urls)),
+    url(r'^shop/', include(shop.urls)),
+    url(r'^panel/', include(panel.urls))
 ]
