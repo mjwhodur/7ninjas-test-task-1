c98caac09550728c180e80ced1ed16339ec7bbd0
Michal Weglowski-Hodur <mjwhodur@gmail.com>
2018-10-17 17:42:56 +0200

    Functions in function-based view

--- ninja_shop/panel/views.py
+++ ninja_shop/panel/views.py
@@ -12,14 +12,13 @@ Also, each view is secured to be viewed only by staff members.
 
 This file is considered that shall be small and concise and provide basic logic
 about views so each controller logic has been put into submodule validators.
+
+Why Class Views are not used? Because function views are easier to control and are more extensible
 """
 
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-
-# Create your views here.
-from panel.validators import deliveryaddress, contractor, deliverymethod, order, prices
 
 
 def panel_login(request):
@@ -49,351 +48,80 @@ def panel_login(request):
                           context={'Error': 'Credentials were not correct. Please try again.'})
 
 
-@staff_member_required
-def category_add(request):
-    """
-    Adding category view
-    :param request: HttpRequest - Incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def product_remove(request, index):
+    return None
 
 
-@staff_member_required
-def category_edit(request, category):
-    """
-    Edit category view
-    :param request: HttpRequest - Incoming request
-    :param category: int
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def product_edit(request, index):
+    return None
 
 
-@staff_member_required
-def category_remove(request):
-    """
-    Remove category view
-    :param request: HttpRequest - Incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def product_view(request, index):
+    return None
 
 
-@staff_member_required
 def product_add(request):
-    """
-    Add product view
-    :param request: HttpRequest - Incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+    return None
 
 
-@staff_member_required
-def product_edit(request):
-    """
-    Edit product data view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def product_list(request):
+    return None
 
 
-@staff_member_required
-def product_remove(request):
-    """
-    Remove product view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def order_remove(request, index):
+    return None
 
 
-@staff_member_required
-def currency_add(request):
-    """
-    Add currency view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
+def order_edit(request, index):
+    return None
+
+
+def order_add(request):
+    return None
+
+def order_view(request, index):
     pass
 
 
-@staff_member_required
-def currency_edit(request):
-    """
-    Edit currency view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def order_list(request):
+    return None
 
 
-@staff_member_required
-def currency_remove(request):
-    """
-    Remove currency view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def category_view(request, index):
+    return None
 
 
-@staff_member_required
-def orders_list(request):
-    """
-    List orders view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        pass
+def category_remove(request, index):
+    return None
 
 
-@staff_member_required
-def orders_add(request):
-    """
-    Add orders view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return order.validate_add(request)
+def category_edit(request, index):
+    return None
 
 
-@staff_member_required
-def orders_edit(request):
-    """
-    Edit order with id... view
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return order.validate_edit(request)
+def category_add(request):
+    return None
 
 
-@staff_member_required
-def orders_remove(request):
-    """
-    Remove order with id...
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return order.validate_remove(request)
+def category_list(request):
+    return None
 
 
-@staff_member_required
-def orders_fullfill(request):
-    """
-    Fullfill order with ID and set its state to 'fulfilled'
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return order.validate_fullfil(request)
+def delivery_method_view(request, index):
+    return None
 
 
-@staff_member_required
-def deliverymethod_list(request):
-    """
-    Show delivery methods with their prices
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliverymethod.validate_list(request)
+def delivery_method_remove(request, index):
+    return None
 
 
-@staff_member_required
-def deliverymethod_add(request):
-    """
-    Add delivery methods
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliverymethod.validate_add(request)
+def delivery_method_edit(request, index):
+    return None
 
 
-@staff_member_required
-def deliverymethod_edit(request):
-    """
-    Edit delivery methods
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliverymethod.validate_edit(request)
+def delivery_method_add(request):
+    return None
 
 
-@staff_member_required
-def deliverymethod_remove(request):
-    """
-    Remove delivery methods
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliverymethod.validate_remove(request)
-
-
-@staff_member_required
-def contractor_add(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return contractor.validate_add(request)
-
-
-@staff_member_required
-def contractor_edit(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return contractor.validate_edit(request)
-
-
-@staff_member_required
-def contractor_remove(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return contractor.validate_remove(request)
-
-
-@staff_member_required
-def contractor_list(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return contractor.validate_list(request)
-
-
-@staff_member_required
-def deliveryaddress_add(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliveryaddress.validate_add(request)
-
-
-@staff_member_required
-def deliveryaddress_edit(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliveryaddress.validate_edit(request)
-
-
-@staff_member_required
-def deliveryaddress_remove(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliveryaddress.validate_remove(request)
-
-
-@staff_member_required
-def deliveryaddress_list(request):
-    """
-
-    :param request: HttpRequest - incoming request
-    :return: HttpResponse
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return deliveryaddress.validate_list(request)
-
-
-@staff_member_required
-def set_prices_for_product(request, product_index):
-    """
-
-    :param request:
-    :param product_index:
-    :return:
-    """
-    if request.method == "GET":
-        pass
-    if request.method == "POST":
-        return prices.validate(request, product_index)
+def delivery_method_list(request):
+    return None
